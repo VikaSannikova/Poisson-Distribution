af5898e4bd1c1d6772555b3046a110b05ac9b336
VikaSannikova <vikasannikova1998@gmail.com>
2019-03-17 21:08:23 +0300

    remember about avg, init params for N threads

--- src/main/java/approximation/DoneClasses/Loop.java
+++ src/main/java/approximation/DoneClasses/Loop.java
@@ -40,98 +40,80 @@ public class Loop {
 
     public Loop(int numOfThreads) {
         this.yellowTime = 10.0;
-        Integer queue[] = {1,2,3};
+        Integer queue[] = {1,2,3,4,5};
         this.setQueues(new ArrayList<Integer>(Arrays.asList(queue)));
-        Double times[] = {10.0,10.0,10.0};
+        Double times[] = {10.0,10.0,10.0,10.0,10.0};
         this.setGreenTimes(new ArrayList<Double>(Arrays.asList(times)));
-        Double lambdas[] = {0.01,1.0,1.0}; //интенсивность для 1 потока очеь мала
+        Double lambdas[] = {0.01,1.0,1.0,1.0,3.0}; //интенсивность для 1 потока очень мала
         this.setLambdas(new ArrayList<Double>(Arrays.asList(lambdas)));
-        String strings[] = {"x^2", "x^2", "x^2"};
+        String strings[] = {"x^2", "x^2", "x^2", "x^2", "x^2"};
         this.setFormulas(new ArrayList<String>(Arrays.asList(strings)));
-        Double timesRed[] = {5.0,5.0,5.0};
+        Double timesRed[] = {5.0,5.0,5.0,5.0,5.0};
         this.setRedTimes(new ArrayList<Double>(Arrays.asList(timesRed)));
 
-        threads.add(new Thread(queues.get(0), greenTimes.get(0), this.lambdas.get(0), new Formula(formulas.get(0)),9));
-        threads.get(0).createQueue();
-        System.out.println("Queue Thread 1 after 1GL: " + threads.get(0).getQueue());
-        //считаем сколько придет по 2 потоку за время работы 1
-        threads.add(new Thread(queues.get(1), greenTimes.get(0), this.lambdas.get(1), new Formula(formulas.get(1)), 9));
-        threads.get(1).createQueueWithoutService();
-        //считаем сколько придет по 3 потоку за время работы 1
-        threads.add(new Thread(queues.get(2), greenTimes.get(0), this.lambdas.get(2), new Formula(formulas.get(2)), 9));
-        threads.get(2).createQueueWithoutService();
-
-        System.out.println("Queues: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-
-        System.out.println("_______________________________");
-        //работа за 1 КС
-        threads.get(0).setT(redTimes.get(0));
-        threads.get(0).createQueueWithoutService();
-        threads.get(1).setT(redTimes.get(0));
-        threads.get(1).createQueueWithoutService();
-        threads.get(2).setT(redTimes.get(0));
-        threads.get(2).createQueueWithoutService();
-        System.out.println("Queues: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-        System.out.println("_______________________________");
-        //работа за 2 ЗС
-        System.out.println("Queues after 2GL");
-        threads.get(0).setT(greenTimes.get(1));
-        threads.get(0).createQueueWithoutService();
-        threads.get(1).setT(greenTimes.get(1));
-        threads.get(1).createQueue();
-        System.out.println("Queue Thread 2 after 2GL: " + threads.get(1).getQueue());
-        threads.get(2).setT(greenTimes.get(1));
-        threads.get(2).createQueueWithoutService();
-        System.out.println("Queues: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-        System.out.println("_______________________________");
-        //работа за 2 КС
-        threads.get(0).setT(redTimes.get(1));
-        threads.get(0).createQueueWithoutService();
-        threads.get(1).setT(redTimes.get(1));
-        threads.get(1).createQueueWithoutService();
-        threads.get(2).setT(redTimes.get(1));
-        threads.get(2).createQueueWithoutService();
-        System.out.println("Queues after 2 RL: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-        System.out.println("_______________________________");
-        //работа за 3 ЗС
-        threads.get(0).setT(greenTimes.get(2));
-        threads.get(0).createQueueWithoutService();
-        threads.get(1).setT(greenTimes.get(2));
-        threads.get(1).createQueueWithoutService();
-        threads.get(2).setT(greenTimes.get(2));
-        threads.get(2).createQueue();
-        System.out.println("Queues after 3 GL: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-        System.out.println("_______________________________");
-        //петля
-        int i = 0;
-        while(threads.get(0).getQueue()==0){
-            threads.get(0).setT(yellowTime);
-            threads.get(0).createQueueWithoutService();
-            threads.get(1).setT(yellowTime);
-            threads.get(1).createQueueWithoutService();
-            threads.get(2).setT(yellowTime);
-            threads.get(2).createQueue();
-            System.out.println("Queues after "+ i +" YL: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-            i++;
+        for(int i = 0; i<numOfThreads; i++){
+            this.threads.add(new Thread(this.queues.get(i), greenTimes.get(0), this.lambdas.get(i), new Formula(formulas.get(i)), 9));
         }
-        System.out.println("Queues after YLs: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
-        System.out.println("_______________________________");
-        //переналадка на обслуживание 1
-        threads.get(0).setT(redTimes.get(2));
-        threads.get(0).createQueueWithoutService();
-        threads.get(1).setT(redTimes.get(2));
-        threads.get(1).createQueueWithoutService();
-        threads.get(2).setT(redTimes.get(2));
-        threads.get(2).createQueueWithoutService();
-        System.out.println("Queues after 3 RL: "+threads.get(0).getQueue() +" " + threads.get(1).getQueue() +" "+threads.get(2).getQueue());
+    }
 
+    public void start(){
+        int k = 0;
+        //работа до последнего зеленого света
+        for(int i = 0; i < numOfThreads-1;i++){
+            for(int j = 0;j<numOfThreads;j++){
+                if(j==k){
+                    threads.get(j).createQueue();
+                    System.out.println("Очередь "+j+" потока = "+ threads.get(j).getQueue()+"после зеленого света"+ i);
+                }else {
+                    threads.get(j).createQueueWithoutService();
+                    System.out.println("Очередь "+j+" потока = "+ threads.get(j).getQueue()+"после зеленого света"+ i);
+                }
+                this.threads.get(j).setT(redTimes.get(i)); //?
+                this.threads.get(j).createQueueWithoutService();
+                System.out.println("Очередь "+j+" потока = "+ threads.get(j).getQueue()+" после красного света"+ i);
+                this.threads.get(j).setT(greenTimes.get(i+1));//?
+            }
+            k++;
+            System.out.println("______________________");
+        }
+        //работа за последний зеленый свет
+        for( int i = 0; i <numOfThreads-1;i++){
+            threads.get(i).createQueueWithoutService();
+            System.out.println("Очередь "+i+" потока = "+ threads.get(i).getQueue()+" после ЗС света" + (numOfThreads-1));
+            threads.get(i).setT(yellowTime);
+        }
+        threads.get(numOfThreads-1).createQueue();
+        System.out.println("Очередь "+(numOfThreads-1)+" потока = "+ threads.get(numOfThreads-1).getQueue()+" после ЗС света" + (numOfThreads-1));
+        threads.get(numOfThreads-1).setT(yellowTime);
 
-
-
-
+        //работа за желтый свет
+        int s = 0;
+        while(threads.get(0).getQueue()==0) {
+            for (int i = 0; i < numOfThreads - 1; i++) {
+                this.threads.get(i).createQueueWithoutService();
+                System.out.println("Очередь "+i+" потока = "+ threads.get(i).getQueue()+" после желтого света" + s);
+                //this.threads.get(i).setT(yellowTime);
+            }
+            this.threads.get(this.numOfThreads - 1).createQueue();
+            System.out.println("Очередь "+(numOfThreads - 1)+" потока = "+ threads.get(numOfThreads - 1).getQueue()+" после желтого света" + s);
+            s++;
+            //this.threads.get(this.numOfThreads - 1).setT(yellowTime);
+        }
+        //работа за последний красный свет
+        for(int i = 0; i < numOfThreads; i++){
+            this.threads.get(i).setT(redTimes.get(numOfThreads-1));
+            this.threads.get(i).createQueueWithoutService();
+            System.out.println("Очередь "+i+" потока = "+ threads.get(i).getQueue()+" после красного света"+ (numOfThreads-1));
+        }
+        System.out.print("Очереди на новое начало цикла: ");
+        for(int i = 0; i<numOfThreads; i++){
+            System.out.print(threads.get(i).getQueue()+", ");
+        }
     }
 
     public static void main(String[] args) {
-        Loop loop = new Loop(3);
+        Loop loop = new Loop(5);
+        loop.setNumOfThreads(5);
+        loop.start();
     }
 }
